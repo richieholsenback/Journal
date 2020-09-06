174e8d3b5a95994b379e03697591e547e7587f21
Richie Holsenback <richieholsenback@gmail.com>
2020-09-06 11:58:21 -0500

    working on adding API functionality

--- scripts/Journal/JournalDataProvider.js
+++ scripts/Journal/JournalDataProvider.js
@@ -1,28 +1,28 @@
-  
-const journal = [
-    {
-        id: 1,
-        date: "07/24/2020",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Ok"
-    },
-    {
-        id: 2,
-        date: "08/24/2020",
-        concept: "JS",
-        entry: "We began a group project to help us solidify our knowledge of automation.",
-        mood: "Good"
-    },
-    {
-        id: 3,
-        date: "08/25/2020",
-        concept: "JS",
-        entry: "We worked on a group project and learned about array methods like .map, .join, .forEach.",
-        mood: "Good, tired"
-    },
-]
+const eventHub = document.querySelector(".container")
 
-export const useJournalEntries = () => {
-    return journal.slice()
+const dispatchStateChangeEvent = () => {
+    const noteStateChangedEvent = new CustomEvent("noteStateChanged")
+
+    eventHub.dispatchEvent(noteStateChangedEvent)
+}
+
+const getNotes = () => {
+    return fetch('http://localhost:8088/entries')
+        .then(response => response.json())
+        .then(parsedNotes => {
+            notes = parsedNotes
+        })
+
+}
+
+export const saveNote = note => {
+    return fetch('http://localhost:8088/notes', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(note)
+    })
+    .then(getNotes)
+    .then(dispatchStateChangeEvent)
 }
--- scripts/Journal/JournalEntry.js
+++ scripts/Journal/JournalEntry.js
@@ -13,3 +13,4 @@ export const JournalEntryComponent = (entry) => {
         </section>
     `
 }
+
